e28f8440c282b453caf8991e5907a1a420be3082
Dana <kyokatsukirayla@gmail.com>
2023-10-22 10:06:57 -0600

    Primera versión

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,51 @@
 use gtk::prelude::*;
 use gtk::{gio, glib, Application};
-use gtk::ApplicationWindow;
-use gtk::Button;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-const APP_ID: &str = "org.gtk_rs.lineas";
+const APP_ID: &str = "org.manzana.lineas";
 fn main() -> glib::ExitCode{
-    //Crear la aplicación
+    // Register and include resources
+    gio::resources_register_include!("resources.gresource")
+        .expect("Failed to register resources.");
+
+    //Create the app
     let app = Application::builder().application_id(APP_ID).build();
     app.connect_startup(|_| {
         let _ = adw::init();
     });
     app.connect_activate(build_ui);
-    //Ejecutar la aplicación i(idwin) dont speking english estube aqui jajajaja
+
+    //Run the app i(idwin) dont speking english estube aqui jajajaja
     app.run()
 }
 
 fn build_ui(app: &Application){
-     let button = Button::builder()
-        .label("Seleccionar Archivo")
-        .margin_top(12)
-        .margin_bottom(12)
-        .margin_start(12)
-        .margin_end(12)
-        .build();
-    let label = gtk::Text::new();
-    label.set_placeholder_text(Some("Hola"));
-    //Create a window and set the title
-    let window = ApplicationWindow::builder()
-        .application(app)
-        .title("Líneas")
-        .child(&button)
-        .build();
-
+    //Load the user interface
+    let ui_src = include_str!("resources/window.ui");
+    let builder = gtk::Builder::from_string(ui_src);
+    let window = builder
+        .object::<gtk::Window>("window")
+        .expect("Couldn't get window");
+    let button = builder
+        .object::<gtk::Button>("button1")
+        .expect("Couldn't get button");
+    let texto = builder
+        .object::<gtk::Label>("texto")
+        .expect("Couldn't get label");
+    window.set_application(Some(app));
     let window_clone = window.clone();
-    // Connect to "clicked" signal of `button`
-    button.connect_clicked(move |button| {
-            //Build the FileDialog object
+        button.connect_clicked(move |_| {
+            /*Create the FileDialog to open the file */
             let dialog = gtk::FileDialog::builder()
                 .title("Seleccione el archivo de texto")
                 .accept_label("Open")
                 .build();
             /*Filters for type text on the FileDialog */
+            let texto = builder
+                .object::<gtk::Label>("texto")
+                .expect("Couldn't get label");
+            /*Add filters to let the user choose only text files */
             let filter = gtk::FileFilter::new();
             filter.add_mime_type("text/plain");
             filter.add_pattern("*.txt");
@@ -50,17 +53,21 @@ fn build_ui(app: &Application){
             let list_filters = gio::ListStore::new::<gtk::FileFilter>();
             list_filters.append(&filter);
             dialog.set_filters(Some(&list_filters));
-            let button_clone = button.clone();
+            dialog.set_default_filter(Some(&filter));
+            /*Opens the dialog to let the user select a file*/
             dialog.open(Some(&window_clone), gio::Cancellable::NONE, move |file| {
                     if let Ok(file) = file {
+                        /*Counts the number of lines in that file */
                         let filename = file.path().expect("No se pudo obtener la ruta");
-                        let file = File::open(filename).expect("No se pudo obtener el archivo");
+                        let file = File::open(&filename).expect("No se pudo obtener el archivo");
                         let file = BufReader::new(file);
                         let mut num_lineas = 0;
                         for _lines in file.lines(){
                             num_lineas += 1;
                         }
-                        button_clone.set_label((num_lineas.to_string()).as_str());
+                        /*Change the label to show the number of lines */
+                        let resultado = format!("El archivo tiene {} líneas de texto",num_lineas);
+                        texto.set_text(resultado.as_str());
                     }
             });
         }
